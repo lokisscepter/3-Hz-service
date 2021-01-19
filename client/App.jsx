d44aa0f3cfa8d7c665acfc994ea4e7905bd514ac
3-Hz <edward.h.zhao@gmail.com>
2021-01-19 14:02:50 -0600

    set up React state

--- client/App.jsx
+++ client/App.jsx
@@ -10,26 +10,92 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      info: null,
+      styles: null,
+      sizes: null,
+      photos: null,
+      current_style: null
+    };
+
+    this.fetchInfo = this.fetchInfo.bind(this);
+    this.fetchStyles = this.fetchStyles.bind(this);
+    this.fetchSizes = this.fetchSizes.bind(this);
+    this.fetchPhotos = this.fetchPhotos.bind(this);
   }
 
   randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
+  fetchInfo(item_id) {
+    return axios.get(`api/items/${item_id}/info`)
+      .then(response => {
+        let item = response.data[0];
+        this.setState({
+          info: {
+            id: item.id,
+            name: item.name,
+            brand: item.brand,
+            avg_rating: item.avg_rating,
+            num_ratings: item.num_ratings
+          }
+        })
+      })
+  }
+
+  fetchStyles(item_id) {
+    return axios.get(`api/items/${item_id}/styles`)
+      .then(response => {
+        let styles = response.data;
+        this.setState({
+          styles: styles,
+          current_style: styles[0]
+        })
+      })
+  }
+
+  fetchSizes(item_id) {
+    return axios.get(`api/items/${item_id}/sizes`)
+      .then(response => {
+        let sizes = response.data;
+        this.setState({
+          sizes: sizes
+        })
+      })
+  }
+
+  fetchPhotos(item_id) {
+    return axios.get(`api/items/${item_id}/photos`)
+      .then(response => {
+        let photos = response.data;
+        this.setState({
+          photos: photos
+        })
+      })
+  }
+
   componentDidMount() {
     let item = this.randInt(1, 12);
-    axios.get(`api/items/${item}/info`)
-      .then(response => {
-        console.log(response.data);
-      })
+    this.fetchInfo(item)
+      .then(() => this.fetchStyles(item))
+      .then(() => this.fetchSizes(item))
+      .then(() => this.fetchPhotos(item))
+      .catch(err => console.log(err));
   }
 
   render() {
     return (
       <div className={styles.app}>
-        <Preview />
-        <Checkout />
+        <Preview
+          photos={this.state.photos}
+          style={this.state.current_style}
+        />
+        <Checkout
+          info={this.state.info}
+          styles={this.state.styles}
+          sizes={this.state.sizes}
+        />
       </div>
     );
   }
