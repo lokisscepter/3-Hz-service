02ce2c23b1627bd808c6154d9823a5d245d3eaf0
3-Hz <edward.h.zhao@gmail.com>
2021-01-23 14:01:14 -0600

    carousel visible w overflow: auto

--- client/components/preview/Carousel.jsx
+++ client/components/preview/Carousel.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import styles from '../../styles/preview/Carousel.module.css';
 
 const Carousel = (props) => {
   return (
     <div>
-      Carousel
+      <div className={styles.container}>
+        {props.photos.map(photo => (
+          <div className={styles.image_wrapper}>
+            <img className={styles.image} src={photo.url_thumbnail}/>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
