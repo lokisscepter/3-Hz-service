2886715b4ad7fb7f7f24d90b513c102ca49a110f
3-Hz <edward.h.zhao@gmail.com>
2021-01-23 13:05:13 -0600

    removed react-image-zoom

--- client/components/preview/Zoom.jsx
+++ client/components/preview/Zoom.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styles from '../../styles/preview/Zoom.module.css';
-import ReactImageZoom from 'react-image-zoom';
 
 const Zoom = (props) => {
   let photo = props.photo || {url_regular: 'https://media.wired.com/photos/5f87340d114b38fa1f8339f9/master/w_1600%2Cc_limit/Ideas_Surprised_Pikachu_HD.jpg'};
@@ -13,7 +12,6 @@ const Zoom = (props) => {
           onMouseMove={(e) => props.zoomIn(e)}
           onMouseOut={(e) => props.zoomOut(e)}
         />
-        {/* <ReactImageZoom className={styles.image} width={750} height={500} zoomWidth={400}  img={photo.url_regular}/> */}
       </div>
   );
 }
