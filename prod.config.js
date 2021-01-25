d7a8816276160ad439d239cc455ad697110b98b6
3-Hz <edward.h.zhao@gmail.com>
2021-01-25 14:09:41 -0600

    added production build and start commands

---
+++ prod.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [{loader: "file-loader"}]
+      }
+    ]
+  },
+  watch: true
+}
