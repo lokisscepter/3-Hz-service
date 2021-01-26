12dd658c804b6df1bbcfdfa5047e8d5f34d017cf
3-Hz <edward.h.zhao@gmail.com>
2021-01-26 13:52:15 -0600

    moved compiled files into .gitignored dist dir

--- prod.config.js
+++ prod.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "production",
   entry: "./client/index.js",
   output: {
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "public/dist"),
     filename: "bundle.js"
   },
   module: {
