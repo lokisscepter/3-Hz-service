d02d8ebdb36d2c3f3b9fa6b4dd54e49372a4e347
Berret Terry <berretterry@gmail.com>
2021-02-05 19:28:55 -0600

    need a branch

--- new-db/db.js
+++
@@ -1,9 +0,0 @@
-const pg = require('pg');
-const Promise = require('bluebird');
-const credentials = require('./db.config.js');
-
-const connection = MySql.createConnection(credentials)
-
-const db = Promise.promisifyAll(connection, { multiArgs: true });
-
-module.exports = db;
